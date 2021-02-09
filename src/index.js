0b81b98a43c57e69fb2a1d2e052d772560439188
Melissa Faulner <melissa.faulner@gmail.com>
2021-02-09 09:57:31 -0500

    fetch functionality now working to renderOneRamen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-// write your code here
+
+// VARIABLES
+const url = 'http://localhost:3000/ramens'
+
+const ramenMenu = document.querySelector('#ramen-menu')
+const ramenDetail = document.querySelector('#ramen-detail')
+const ramenRating = document.querySelector('#ramen-rating')
+
+fetchRamens();
+
+// FUNCTIONS
+
+function fetchRamens() {
+    fetch(url)
+    .then(response => response.json())
+    .then(displayAllRamen)
+}
+
+function displayAllRamen(ramenArr) {
+    ramenArr.forEach(ramen => {
+        renderOneRamen(ramen)
+    })
+}
+
+function renderOneRamen(ramen) {
+    const image = document.createElement('img')
+    console.log(ramen.name);
+    image.innerHTML = ramen.image
+}
+
+// 
